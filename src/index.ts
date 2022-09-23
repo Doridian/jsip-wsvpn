dd6f381eb90586cdcaa4bd8289fd844e80233f1c
Doridian <git@doridian.net>
2022-09-23 15:30:24 -0700

    jsip 1.6.5

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { WSVPNJSIP } from "./bridge";
 import { WSVPNErrorEvent, WSVPNWebSocket } from "@wsvpn/js";
 import { initialize } from "@doridian/jsip";
-import { enableTCP, TCPConn } from "@doridian/jsip/lib/ethernet/ip/tcp/stack";
+import { enableTCP, enableTCPEcho, TCPConn } from "@doridian/jsip/lib/ethernet/ip/tcp/stack";
 import { dnsResolveOrIp } from "@doridian/jsip/lib/ethernet/ip/udp/dns/stack";
 
 const win = (window as any);
@@ -9,6 +9,7 @@ const win = (window as any);
 export async function main() {
     initialize();
     enableTCP();
+    enableTCPEcho();
 
     const transport = new WSVPNWebSocket("ws://10.99.10.1:9000");
     transport.addEventListener("error", (ev: WSVPNErrorEvent) => {
